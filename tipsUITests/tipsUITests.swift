8f09bc7bc2646cb3ecfb17ae64776a7f734b2096
shah12121 <apurva1@umbc.edu>
2016-12-24 01:10:24 -0500

    Update tipsUITests.swift

--- tipsUITests/tipsUITests.swift
+++ tipsUITests/tipsUITests.swift
@@ -32,5 +32,5 @@ class tipsUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+     
 }
